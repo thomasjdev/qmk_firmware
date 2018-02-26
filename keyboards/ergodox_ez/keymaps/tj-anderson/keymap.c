5a84bf7c4cc8e6efdf8eaaf0210f4fdcace5f27e
Thomas Anderson <thomas@tjanderson.net>
2018-02-26 10:37:33 -0800

    Forgot to update one key

--- keyboards/ergodox_ez/keymaps/tj-anderson/keymap.c
+++ keyboards/ergodox_ez/keymaps/tj-anderson/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         CTL_ESC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_Z,        ALT_X,   GUI_C,   KC_V,   KC_B,   KC_LBRC,
         KC_LCTL,         KC_FNL,      KC_LCTL,  KC_LALT, KC_LGUI,
-                                                     KC_LCBR,  KC_HYPR,
+                                                     KC_LPRN,  KC_HYPR,
                                                                KC_LABK,
                                                KC_BSPC,KC_DELT,GUI_SPC,
         // right hand
