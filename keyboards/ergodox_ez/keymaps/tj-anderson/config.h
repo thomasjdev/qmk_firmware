1f711435e01f83ccee53a0cedc895534a9e61a02
Thomas Anderson <thomas@tjanderson.net>
2018-04-11 14:42:35 -0700

    Adjusted the timeout for holding keys

--- keyboards/ergodox_ez/keymaps/tj-anderson/config.h
+++ keyboards/ergodox_ez/keymaps/tj-anderson/config.h
@@ -1,10 +1,10 @@
 #include "../../config.h"
 
-// #undef DEBOUNCE
-// #define DEBOUNCE 5
+#undef DEBOUNCE
+#define DEBOUNCE 5
 
-// #undef TAPPING_TERM
-// #define TAPPING_TERM 300
+#undef TAPPING_TERM
+#define TAPPING_TERM 300
 
 // #define DISABLE_SPACE_CADET_ROLLOVER
 #define IGNORE_MOD_TAP_INTERRUPT
