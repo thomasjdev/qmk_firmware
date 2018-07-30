7fea027bcf6660a840b4cf86adf41d4027142811
Thomas Anderson <thomas@tjanderson.net>
2018-07-30 08:59:53 -0700

    Trying some new changes to help typing speed

--- keyboards/ergodox_ez/keymaps/tj-anderson/config.h
+++ keyboards/ergodox_ez/keymaps/tj-anderson/config.h
@@ -1,12 +1,12 @@
 #include "../../config.h"
 
-#undef DEBOUNCE
-#define DEBOUNCE 5
+// #undef DEBOUNCE
+// #define DEBOUNCE 5
 
 // #undef TAPPING_TERM
 // #define TAPPING_TERM 200
 
-#define PERMISSIVE_HOLD
-#define DISABLE_SPACE_CADET_ROLLOVER
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define PERMISSIVE_HOLD
+// #define DISABLE_SPACE_CADET_ROLLOVER
+// #define IGNORE_MOD_TAP_INTERRUPT
 #define RETRO_TAPPING
