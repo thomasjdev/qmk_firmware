b381df6e7ee97c295a916f279f7d623c39a8abe2
Thomas Anderson <thomas@tjanderson.net>
2018-08-02 13:26:56 -0700

    Fix wrong slash being sent in layer

--- keyboards/planck/keymaps/tj-anderson/keymap.c
+++ keyboards/planck/keymaps/tj-anderson/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_MINS,   KC_PLUS,   _______,   KC_SLSH,   _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LPRN, KC_RPRN,
+    KC_DEL,  KC_MINS,   KC_PLUS,   _______,   KC_BSLS,   _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LPRN, KC_RPRN,
     _______, KC_UNDS,   KC_EQL,   _______,   KC_PIPE,  _______,  _______,  _______, KC_PGUP, KC_PGDN, KC_LBRC, KC_RBRC,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END, KC_LCBR, KC_RCBR
 ),
