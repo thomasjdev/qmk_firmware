470cf819b985aecfd715869d42984b8af7b17106
Thomas Anderson <thomas@tjanderson.net>
2018-10-30 15:13:35 -0700

    Reorg the F keys

--- keyboards/ergodox_ez/keymaps/tj-anderson/keymap.c
+++ keyboards/ergodox_ez/keymaps/tj-anderson/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  {   |           |  }   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  Ct  |  AG  |  GS  | Alt  |  Gui |                                       |   Mv | PgUp | PgDn |  Hm  |  End |
+ *   |  FN  |  AG  |  Ct  | Alt  |  Gui |                                       |   Mv | PgUp | PgDn |  Hm  |  End |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |   [  |   _  |       |   -  |   ]  |
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         CUST_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LPRN,
         HYP_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LCBR,
-        KC_LCTL, ALT_G, GUI_S, KC_LALT, KC_LGUI,
+        KC_FNL, KC_MOVE, KC_LCTL, KC_LALT, KC_LGUI,
         KC_LBRC, KC_UNDS,
         KC_LABK,
         KC_BSPC, KC_DELT, GUI_SPC,
@@ -116,11 +116,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |      |      |      |      |      | F12  |           |  F13 |      |      |      |      | Play |  Vol+  |
+ * |         |      |      |      |      |      |   <  |           |   >  |      |      |      |      |      |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |      |      |      |      |------|           |------|   L  |   D  |   U  |   R  | Next |  Vol-  |
- * |---------+------+------+------+------+------| F14  |           |  F15 |------+------+------+------+------+--------|
- * |         |      |      |      |      |      |      |           |      |      |      |      |      | Prev |  Mute  |
+ * |         |      |      |      |      |      |------|           |------|   L  |   D  |   U  |   R  |      |   F14  |
+ * |---------+------+------+------+------+------|   [  |           |   ]  |------+------+------+------+------+--------|
+ * |         |      |      |      |      |      |      |           |      | Mute | Play | Vol+ | Vol -| Prev |   F15  |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -136,18 +136,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [FN] = LAYOUT_ergodox(
         // left hand
         KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LABK,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F14,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_PGDN, KC_PGUP,
         KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,
         // right hand
         KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        KC_F13, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_VOLU,
-        KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_MPRV, KC_VOLD,
-        KC_F15, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_MUTE,
+        KC_RABK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_F12,
+        KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_F14,
+        KC_LBRC, KC_MUTE, KC_MPLY, KC_VOLU, KC_VOLD, KC_MNXT, KC_F15,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_HOME, KC_END,
         KC_TRNS,
