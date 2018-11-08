1d351604def1932f7c1ccaaae11c1736ae4097c4
Thomas Anderson <thomas@tjanderson.net>
2018-11-08 10:24:15 -0800

    Change the keymapping around to make more use of the move layer

--- keyboards/ergodox_ez/keymaps/tj-anderson/keymap.c
+++ keyboards/ergodox_ez/keymaps/tj-anderson/keymap.c
@@ -17,7 +17,7 @@ enum custom_keycodes {
 #define GUI_SPC     LGUI(KC_SPC)
 #define KC_FNL      MO(FN)
 #define KC_MOVE     MO(MOVE)
-
+#define KC_CESC     LCAG_T(KC_ESC)
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
@@ -27,16 +27,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `    |   1  |   2  |   3  |   4  |   5  |   _  |           |  -   |   6  |   7  |   8  |   9  |   0  |    =   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |   (  |           |   )  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  Pgup|           |  Hm  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Hyp/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------|  {   |           |  }   |------+------+------+------+------+--------|
+ * | CAG/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |--------+------+------+------+------+------|  Pgdn|           |  End |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  FN  |  AG  |  Ct  | Alt  |  Gui |                                       |   Mv | PgUp | PgDn |  Hm  |  End |
+ *   |  Ctl |  Alt |  Cmd |  FN  |  MV  |                                       |   MV |   FN | Cmd  |  Alt |  Ctl |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |   [  |   _  |       |   -  |   ]  |
+ *                                        |   -  |   [  |       |   ]  |   _  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |  <   |       |   >  |        |      |
  *                                 |BkSpc |  Del |------|       |------|  Ent   |  Spc |
@@ -48,18 +48,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ergodox( // layer 0 : default
         // left hand
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_UNDS,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LPRN,
-        KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LCBR,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_PGUP,
+        KC_CESC, KC_A, KC_S, KC_D, KC_F, KC_G,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_PGDN,
         KC_LCTL, KC_LALT, KC_LGUI, KC_FNL, KC_MOVE,
         KC_MINS, KC_LBRC,
         KC_LABK,
         KC_BSPC, KC_DELT, GUI_SPC,
         // right hand
         KC_MINS, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,
-        KC_RPRN, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+        KC_HOME, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
         KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        KC_RCBR, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_END, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
         KC_MOVE, KC_FNL, KC_RGUI, KC_RALT, KC_RCTL,
         KC_RBRC, KC_UNDS,
         KC_RABK,
@@ -67,13 +67,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap 1: Move & Control Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
- * |         |      |      |      |      |      |      |           |      |      |      |      |      |     |  PgUp   |
+ * |         |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |      |   7  |   8  |   9  |      |      |           |      |      |      |      |      |      |  PgDn  |
+ * |     [   |      |   7  |   8  |   9  |      |      |           |      |      |      |      |      |      |    ]   |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |   4  |   5  |   6  |      |------|           |------|   L  |   D  |  U   |   R  |      |  Home  |
- * |---------+------+------+------+------+------|   [  |           |   ]  |------+------+------+------+------+--------|
- * |         |      |   1  |   2  |   3  |  0   |      |           |      |      |      |      |      |      |  End   |
+ * |     {   |      |   4  |   5  |   6  |      |------|           |------|   L  |   D  |  U   |   R  |      |    }   |
+ * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |     (    |      |   1  |   2  |   3  |  0  |      |           |      |      |      |      |      |      |    )   |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -87,19 +87,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [MOVE] = LAYOUT_ergodox(
         // left hand
-        KC_TRNS, LCTL(LGUI(S(KC_3))), LCTL(LGUI(S(KC_4))), KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-        KC_TRNS, KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_LCBR,
-        KC_TRNS, KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_1, KC_2, KC_3, KC_0, KC_LBRC,
+        KC_TRNS, LCTL(LGUI(S(KC_3))), LCTL(LGUI(S(KC_4))), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_LBRC, KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_PGUP,
+        KC_LCBR, KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS,
+        KC_LPRN, KC_TRNS, KC_1, KC_2, KC_3, KC_0, KC_PGDN,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_PGDN, KC_PGUP,
         KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,
         // right hand
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP,
-        KC_RCBR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN,
-        KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_HOME,
-        KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RBRC,
+        KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_RCBR,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RPRN,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_HOME, KC_END,
         KC_TRNS,
